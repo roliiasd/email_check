47cca14647c350551fd0fcc1d77f8812b9b595eb
Kovács Roland <59372616+roliiasd@users.noreply.github.com>
2025-02-13 12:37:06 +0100

    Add files via upload

---
+++ Email_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Email_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Email_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a55c1378-665e-4c2f-8389-bcb99c1e53bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Email_Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Email;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Email_Tests
+{
+    [TestClass]
+    public class Email_Validator_Tests
+    {
+        [TestMethod]
+        public void IsValidEmail_ReturnsTrue()
+        {
+            var validator = new Email_Validator();
+
+            bool result = validator.IsValidEmail("test@test.com");
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void EmailWithoutAtSymbol_ReturnsFalse()
+        {
+            var validator = new Email_Validator();
+
+            bool result = validator.IsValidEmail("testtest.com");
+
+            Assert.IsFalse(result);
+        }
+    }
+}
